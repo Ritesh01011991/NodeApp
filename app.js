2bea08cec843a49ff57fa89c1bc85740cf2db679
Ritesh01011991 <35454499+Ritesh01011991@users.noreply.github.com>
2019-12-28 15:56:17 +0530

    ok

--- app.js
+++ app.js
@@ -2,14 +2,17 @@ var express = require('express');
 var app = express();
 var fs = require("fs");
 
-app.get('/listUsers', function (req, res) {
+app.set('port', (process.env.PORT || 5000 ));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
   
       res.end( "Hey Ritesh this is your First Heroku Project ! Good Luck" );
-   });
+  
 })
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+app.listen(app.get('port'), function (){
+	console.log('Node App running fine' , app.get('port'))
+});
+
+
